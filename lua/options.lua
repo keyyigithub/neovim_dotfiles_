73c15aa68d90f332105082976ecda84d37028287
keyyi <2023748942@qq.com>
2025-02-13 19:22:19 +0800

    对主题进行了些许改动。

--- lua/options.lua
+++ lua/options.lua
@@ -36,7 +36,7 @@ api.nvim_create_autocmd("FileType", {
   pattern = "java",
   callback = function ()
     local config = {
-      cmd = {'/path/to/jdt-language-server/bin/jdtls'},
+      cmd = {'/usr/share/java/jdtls/bin/jdtls'},
       root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
     }
     require('jdtls').start_or_attach(config);
