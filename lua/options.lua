dc822e1ef3f5f744e54501ce2d23fddacc76bcd2
keyyi <2023748942@qq.com>
2025-02-15 12:34:28 +0800

    修改了lsp的调用方式。

--- lua/options.lua
+++ lua/options.lua
@@ -25,38 +25,10 @@ o.relativenumber = true
 --autocmds
 
 --loads the language servers
--- **lua_ls is loaded automatically
-local lsp = api.nvim_create_augroup("LSPLoad", {
-  clear = false,
-})
-
---java
-api.nvim_create_autocmd("FileType", {
-  group = lsp,
-  pattern = "java",
-  callback = function ()
-    local config = {
-      cmd = {'/usr/share/java/jdtls/bin/jdtls'},
-      root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-    }
-    require('jdtls').start_or_attach(config);
-  end
-})
-
---tex
-api.nvim_create_autocmd("FileType", {
-  group = lsp,
-  pattern = { "tex", "plaintex", "bib" },
-  callback = function ()
-    require('lspconfig').texlab.setup{};
-  end
-})
 
 --c/c++
-api.nvim_create_autocmd('FileType', {
-  group = lsp,
-  pattern = { "h", "c", "cpp" },
-  callback = function ()
-    require('lspconfig').ccls.setup{};
-  end
-})
+require('lspconfig').ccls.setup{}
+--java
+require('lspconfig').jdtls.setup{}
+--(la)tex
+require('lspconfig').texlab.setup{}
