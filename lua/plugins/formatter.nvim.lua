4ae951c88c9e3deec763c46e8a6923d5044b17bd
keyyi <2023748942@qq.com>
2025-02-28 15:46:21 +0800

    重命名formatter插件

---
+++ lua/plugins/formatter.nvim.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "mhartington/formatter.nvim",
+    config = function ()
+      require("formatter").setup {
+      logging = true,
+      log_level = vim.log.levels.WARN,
+      filetype = {
+        lua = {
+          require("formatter.filetypes.lua").stylua,
+        },
+        ["*"] = {
+          require("formatter.filetypes.any").remove_trailing_whitespace,
+        }
+      }
+    }
+    end,
+    lazy = false,
+  },
+}
