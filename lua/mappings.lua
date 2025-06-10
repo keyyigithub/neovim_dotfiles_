96f018ac92f0d82a49dffd1c7cba5287d6df5fc8
keyyi <2023748942@qq.com>
2025-06-10 17:22:34 +0800

    Add new plugin jieba.nvim; Add some descriptions to keymaps.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,13 +5,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "<ESC><ESC>", ":noh<Return>")
+map("n", "<ESC><ESC>", ":noh<Return>", { desc = "Stop the highlight" })
 
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "Exits the insert mode" })
 
-map("t", "<ESC>", "<C-\\><C-N>")
+map("t", "<ESC>", "<C-\\><C-N>", { desc = "Exit the terminal mode" })
 
 map("n", "<localleader>lt", function()
   return require("vimtex.fzf-lua").run()
-end)
+end, { desc = "Table of contents for latex" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
