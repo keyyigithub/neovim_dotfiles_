2c3ea958eeb7a07b329bcff0c91ac1edb82914a2
keyyi <2023748942@qq.com>
2025-06-12 12:50:48 +0800

    Remove repeated keymaps.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,54 +17,6 @@ end, { desc = "Table of contents for latex" })
 
 -- keymaps for jieba.nvim
 
-vim.keymap.set(
-  { "x", "n" },
-  "B",
-  '<cmd>lua require("jieba_nvim").wordmotion_B()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "b",
-  '<cmd>lua require("jieba_nvim").wordmotion_b()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "w",
-  '<cmd>lua require("jieba_nvim").wordmotion_w()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "W",
-  '<cmd>lua require("jieba_nvim").wordmotion_W()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "E",
-  '<cmd>lua require("jieba_nvim").wordmotion_E()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "e",
-  '<cmd>lua require("jieba_nvim").wordmotion_e()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "ge",
-  '<cmd>lua require("jieba_nvim").wordmotion_ge()<CR>',
-  { noremap = false, silent = true }
-)
-vim.keymap.set(
-  { "x", "n" },
-  "gE",
-  '<cmd>lua require("jieba_nvim").wordmotion_gE()<CR>',
-  { noremap = false, silent = true }
-)
 vim.keymap.set("n", "ce", ":lua require'jieba_nvim'.change_w()<CR>", { noremap = false, silent = true })
 vim.keymap.set("n", "de", ":lua require'jieba_nvim'.delete_w()<CR>", { noremap = false, silent = true })
 vim.keymap.set("n", "<leader>w", ":lua require'jieba_nvim'.select_w()<CR>", { noremap = false, silent = true })
