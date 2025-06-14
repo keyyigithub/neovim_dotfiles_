f9de8effa934d0a27a8c5da11c0333a745fbde00
keyyi <2023748942@qq.com>
2025-06-15 00:42:32 +0800

    Change the way to add lsps according to new standards of nvim-lspconfig('https://github.com/neovim/nvim-lspconfig/blob/master/README.md')

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp
 
 -- EXAMPLE
 local servers = { "html", "cssls" }
@@ -9,11 +9,12 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  lspconfig.config(lsp)
 end
 
 -- configuring single server, example: typescript
