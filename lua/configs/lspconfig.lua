2240428d617b2713d9ffe9b07d0e86d4ee746306
keyyi <2023748942@qq.com>
2025-06-21 15:22:07 +0800

    Adds new LSP server rime-ls for Chinese input support.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,22 +4,22 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = vim.lsp
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "cmake", "perlnavigator", "bashls", "pyright" }
+local servers = { "html", "cssls", "clangd", "cmake", "perlnavigator", "bashls", "pyright", "rime-ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+local custom_servers = { "rime-ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig.config(lsp, {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  })
-  lspconfig.enable(lsp)
+  if lsp ~= "rime-ls" then
+    lspconfig.config(lsp, {
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    })
+    lspconfig.enable(lsp)
+  end
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.enable "rime-ls"
