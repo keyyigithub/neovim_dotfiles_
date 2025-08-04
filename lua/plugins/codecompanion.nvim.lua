bc8a306bacd70691ccf76a711f4b97dd8c5ab784
Jason <2023748942@qq.com>
2025-08-04 21:31:56 +0800

    Add plugin codecompanion.nvim.

---
+++ lua/plugins/codecompanion.nvim.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- Basic configurations
+    opts = {
+      adapters = {
+        deepseek = function()
+          return require("codecompanion.adapters").extend("deepseek", {
+            env = {
+              api_key = function()
+                return os.getenv "DEEPSEEK_API_KEY"
+              end,
+            },
+            schema = {
+              temperature = {
+                default = 0,
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "deepseek",
+        },
+        inline = {
+          adapter = "deepseek",
+        },
+        cmd = {
+          adapter = "deepseek",
+        },
+      },
+    },
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    "echasnovski/mini.diff",
+    config = function()
+      local diff = require "mini.diff"
+      diff.setup {
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      }
+    end,
+  },
+}
