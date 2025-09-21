8b328b058aa6e514d942eeb979279ca9f0b005d1
Jason <2023748942@qq.com>
2025-09-21 23:52:46 +0800

    Remove rime-ls config and obsidian.nvim.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,21 +4,17 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = vim.lsp
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "cmake", "perlnavigator", "bashls", "pyright", "rime-ls", "lemminx" }
+local servers = { "html", "cssls", "clangd", "cmake", "perlnavigator", "bashls", "pyright", "lemminx" }
 local nvlsp = require "nvchad.configs.lspconfig"
-
-local custom_servers = { "rime-ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  if lsp ~= "rime-ls" then
-    lspconfig.config(lsp, {
-      on_attach = nvlsp.on_attach,
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-    })
-    lspconfig.enable(lsp)
-  end
+  lspconfig.config(lsp, {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  })
+  lspconfig.enable(lsp)
 end
 
 -- configuring single server, example: typescript
