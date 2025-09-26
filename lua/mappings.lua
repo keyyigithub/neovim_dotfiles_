bd7f2144f49ba6d3fa02f9e1a1898bae450840ee
Jason <2023748942@qq.com>
2025-09-26 15:26:56 +0800

    Remove the key bingdings of jieba.nvim (no longer used)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,10 +15,4 @@ map("n", "<localleader>lt", function()
   return require("vimtex.fzf-lua").run()
 end, { desc = "Table of contents for latex" })
 
--- keymaps for jieba.nvim
-
-vim.keymap.set("n", "ce", ":lua require'jieba_nvim'.change_w()<CR>", { noremap = false, silent = true })
-vim.keymap.set("n", "de", ":lua require'jieba_nvim'.delete_w()<CR>", { noremap = false, silent = true })
-vim.keymap.set("n", "<leader>w", ":lua require'jieba_nvim'.select_w()<CR>", { noremap = false, silent = true })
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
